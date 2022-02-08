98c17f0a021ab6b0d916e8428a715a87c3353c5c
kolynzb <collinsbenda360@gmail.com>
2022-02-08 20:11:19 +0300

    yes sir

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
 const User = require('../models/userModel');
 
-exports.signup = async (req, res) => {}
+exports.signup = async (req, res) => {
+  const newUser = await User.create(req.body);
+  res.status(201).json({ status: 'success', data: { user: newUser } });
+};
